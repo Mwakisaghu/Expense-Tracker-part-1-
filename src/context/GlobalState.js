11df80965589c9bbaf5838c76174780667e5761d
mwakisaghu <harrisonchambela2016@gmail.com>
2022-03-03 13:35:34 +0300

    This gets rid of the dummy state to a blank array state

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,12 +4,7 @@ import { DELETE_TRANSACTION, ADD_TRANSACTION } from './types';
 
 //Initial State
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 },
-  ],
+  transactions: [],
 };
 
 //Create Global context
