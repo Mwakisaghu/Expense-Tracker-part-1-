c88a29403a4572b9d6195847b353fd50ff158a4f
mwakisaghu <harrisonchambela2016@gmail.com>
2022-03-03 11:54:58 +0300

    This changes the styling: income(Green) while Expenses(Red)

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,7 +4,7 @@ export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
-    <li className="minus">
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}{' '}
       <span>
         {sign}Kshs{Math.abs(transaction.amount)}
