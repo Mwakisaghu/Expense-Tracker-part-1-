4dbb3348310889136ce8bd4ecfa1329576a452c3
mwakisaghu <harrisonchambela2016@gmail.com>
2022-03-03 11:23:12 +0300

    This gives the Transaction list access to the Globalcontext, Provider and initial State

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,14 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionsList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <Fragment>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-Khs400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <li className="minus">
+            {transaction.text} <span>-Khs400</span>
+            <button className="delete-btn">x</button>
+          </li>
+        ))}
       </ul>
     </Fragment>
   );
