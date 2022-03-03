764f2f7bb481c27d143cced8d21923c040dcf772
mwakisaghu <harrisonchambela2016@gmail.com>
2022-03-03 13:31:56 +0300

    This adds delete and addTransaction actions along with the type.js file

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,20 @@
 /* eslint-disable */
+import { DELETE_TRANSACTION, ADD_TRANSACTION } from './types';
+
 export default (state, action) => {
   switch (action.type) {
+    case DELETE_TRANSACTION:
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case ADD_TRANSACTION:
+      return {
+        ...state,
+        transactions: [...state.transactions, action.payload],
+      };
     default:
       return state;
   }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,5 +1,6 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
+import { DELETE_TRANSACTION, ADD_TRANSACTION } from './types';
 
 //Initial State
 const initialState = {
@@ -18,8 +19,31 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  //Actions
+  //Delete Transaction action
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: DELETE_TRANSACTION,
+      payload: id,
+    });
+  };
+
+  //Add Transaction action
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: ADD_TRANSACTION,
+      payload: transaction,
+    });
+  };
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
